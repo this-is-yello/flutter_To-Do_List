bbcb3aab31c932e75f5506f39c22ef547f0bcbd8
this-is-yello <yello_3617@naver.com>
2022-10-25 19:03:04 +0900

    221025 in Company

--- flutter_todo_list/lib/provider/todo_default.dart
+++ flutter_todo_list/lib/provider/todo_default.dart
@@ -2,10 +2,10 @@ import 'package:flutter_todo_list/models/todo.dart';
 
 class TodoDefault {
   List<Todo> dummyTodos = [
-    Todo(id: 1, title: '플러터 공부 시작하기', description: '뽕 뽑는 플러터를 읽어봅시다.'),
-    Todo(id: 2, title: '서점가기', description: '뽕 뽑는 플러터를 사러 갈 거예요.'),
-    Todo(id: 3, title: '공원에서 운동하기', description: '집 근처 공원에 가서 안전하게 운동합니다'),
-    Todo(id: 4, title: '출근 준비하기', description: '내일이 월요일이라니..'),
+    Todo(id: 1, title: '데일리 업무', description: '스마트 플레이스, 온라인쇼핑몰 , 채팅상담 문의'),
+    Todo(id: 2, title: '디자인 레퍼런스 카피 작업', description: '디자인 레퍼런스 카피'),
+    Todo(id: 3, title: '고객장부 엑셀 자동화 만들기', description: '고객장부 엑셀 자동화 만들기'),
+    Todo(id: 4, title: '플러터', description: 'to-do 리스트'),
   ];
 
   List<Todo> getTodos() {
