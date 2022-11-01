14818993cc8298c154ddd1145347a6cf0729d70d
this-is-yello <yello_3617@naver.com>
2022-11-01 18:46:59 +0900

    221101 in Company

--- flutter_todo_list/lib/main.dart
+++ flutter_todo_list/lib/main.dart
@@ -1,8 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_list/screens/login_screen.dart';
 import 'package:flutter_todo_list/screens/splash_screen.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(MyApp());
+void main() async{
+
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+
+    WidgetsFlutterBinding.ensureInitialized();
+  print('-- WidgetsFlutterBinding.ensureInitialized');
+
+
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
+runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
